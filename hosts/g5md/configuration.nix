9aa4079397bb7bc5fc621ab9ce200f379c6dbf9e
Daniel Liu <danxliu@protonmail.com>
2026-04-05 19:11:23 -0700

    feat: fish shell config

--- hosts/g5md/configuration.nix
+++ hosts/g5md/configuration.nix
@@ -35,6 +35,7 @@
   users.users.daniel = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    shell = pkgs.fish;
   };
   programs.vim = {
     enable = true;
@@ -46,6 +47,44 @@
   };
   programs.tmux = {
     enable = true;
+  };
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set -g fish_greeting ""
+      set -g fish_key_bindings fish_vi_key_bindings
+    '';
+    promptInit = ''
+      function fish_prompt
+        set_color black -b brwhite
+        echo -n " 󰘧 "
+        set_color normal
+        echo -n " "
+        # Note: Added a space before the parenthesis here for proper fish syntax
+        printf '%s' (prompt_pwd --full-length-dirs 2)
+        echo -n " "
+      end
+
+      function fish_right_prompt
+        set -l git_status (fish_git_prompt)
+        set -l duration "$CMD_DURATION"
+
+        if test -n "$git_status"
+          echo -n "$git_status "
+        end
+
+        if test -n "$duration"
+          set_color black -b brwhite
+          echo -n " $duration"
+          echo -n "ms "
+        end
+        set_color normal
+      end
+
+      function fish_mode_prompt
+        commandline -f repaint
+      end
+    '';
   };
 
   environment.systemPackages = with pkgs; [
--- modules/core/default.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  # Core modules that should be imported by all hosts.
-  # Add shared configurations here.
-}
