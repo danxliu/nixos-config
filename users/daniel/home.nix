db871adf72ea3a6281f342d21389da03099aec91
Daniel Liu <danxliu@protonmail.com>
2026-04-07 11:26:41 -0700

    feat: migrate waybar to nixos

--- users/daniel/home.nix
+++ users/daniel/home.nix
@@ -41,6 +41,73 @@ in
   programs.waybar = {
     enable = true;
     systemd.enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 40;
+        reload_style_on_change = true;
+        modules-left = [
+          "niri/workspaces"
+          "custom/separator"
+          "niri/window"
+        ];
+        modules-center = [ "clock" ];
+        modules-right = [
+          "tray"
+          "custom/separator"
+          "network"
+          "custom/separator"
+          "battery"
+          "custom/separator"
+          "pulseaudio"
+        ];
+
+        "niri/workspaces" = {
+          format = "{value}";
+        };
+        "niri/window" = {
+          format = "{app_id}";
+          icon = true;
+          rewrite = {
+            "^(.*)$" = "<span text_transform='capitalize'>$1</span>";
+          };
+        };
+        "custom/separator" = {
+          format = "|";
+        };
+        clock = {
+          timezone = "America/Los_Angeles";
+          format = "{:%I:%M %p}";
+          tooltip-format = "{:%A, %B %d %Y}";
+        };
+        battery = {
+          format = "BAT: {capacity}%";
+          tooltip-format = "BAT: {capacity}%";
+          format-charging = "BAT: {capacity}%+";
+          tooltip-format-charging = "BAT: {capacity}%+";
+        };
+        network = {
+          format-wifi = "WIFI: {essid}";
+          tooltip-format-wifi = "WIFI: {essid}";
+          format-ethernet = "ETH: {ifname}";
+          tooltip-format-ethernet = "ETH: {ifname}";
+          format-disconnected = "WIFI: DISCONNECTED";
+          tooltip-format-disconnected = "DISCONNECTED";
+        };
+        pulseaudio = {
+          format = "VOL: {volume}%";
+          format-muted = "[VOL: MUTED]";
+          tooltip-format = "VOL: {volume}%";
+          tooltip-format-muted = "MUTED";
+        };
+        tray = {
+          icon-size = 14;
+          spacing = 6;
+        };
+      };
+    };
+    style = ./waybar/style.css;
   };
   programs.firefox = {
     enable = true;
@@ -333,8 +400,6 @@ in
     configFile = {
       "dunst/dunstrc".source = ./dunst/dunstrc;
       "niri/config.kdl".source = ./niri/config.kdl;
-      "waybar/config.jsonc".source = ./waybar/config.jsonc;
-      "waybar/style.css".source = ./waybar/style.css;
       "nvim/init.lua".source = ./nvim/init.lua;
       "nvim/lua/options.lua".source = ./nvim/lua/options.lua;
       "nvim/lua/keybinds.lua".source = ./nvim/lua/keybinds.lua;
