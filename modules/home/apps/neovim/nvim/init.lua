3c88f570f192bc2dbad9b5cc391fa42526da3a4d
Daniel Liu <danxliu@protonmail.com>
2026-04-20 15:18:11 -0700

    feat: lualine follows base16 theme

--- modules/home/apps/neovim/nvim/init.lua
+++ modules/home/apps/neovim/nvim/init.lua
@@ -115,6 +115,7 @@ require("colorizer").setup()
 
 require("lualine").setup({
     options = {
+        theme = 'base16',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = { 'NvimTree' },
