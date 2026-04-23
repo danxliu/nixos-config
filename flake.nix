b783b9c66d7014059cafb6e905355a07197611d1
Daniel Liu <danxliu@protonmail.com>
2026-04-23 14:30:50 -0700

    feat: remove invalid backup file extension

--- flake.nix
+++ flake.nix
@@ -83,7 +83,6 @@
           modules = [
             (./users + "/${user}/default.nix")
             inputs.nix-index-database.homeModules.nix-index
-            { home.backupFileExtension = "bak"; }
           ];
         };
     in
